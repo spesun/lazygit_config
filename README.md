fd2d14ac1987ef6a8cfa74e1887f6d8e9a6352f3
sunww <sunweiwei@guiji.ai>
2026-04-15 14:40:02 +0800

    0415

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+
+
+# requirement
+[yazi](https://yazi-rs.github.io/docs/installation)
+fg
+rg
+fzf
+zoxide 
--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.env.JAVA_HOME = "E:\\Program Files\\Eclipse Adoptium\\jdk-25.0.1.8-hotspot"
+vim.env.PATH = vim.env.JAVA_HOME .. "/bin;" .. vim.env.PATH
+
+vim.opt.guifont = { "JetBrainsMono Nerd Font:h12" }
+-- 启用Nerd Font图标支持（很多配置/插件依赖这个变量）
+vim.g.have_nerd_font = true
